b678c7efb2c4004345d7d08ebf3a0b095bef3dff
Coco <arthurina008@gmail.com>
2024-10-29 22:11:20 -0700

    backend update

--- build/Desktop_arm_darwin_generic_mach_o_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_BookMarket_FILE /Users/coco/Projects/VSprojects/BookMarket-ForCpp/build/Desktop_arm_darwin_generic_mach_o_64bit-Debug/BookMarket)
-set(__QT_DEPLOY_TARGET_BookMarket_TYPE EXECUTABLE)
--- build/Desktop_arm_darwin_generic_mach_o_64bit-Debug/CMakeFiles/BookMarket_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/BookMarket_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BookMarket_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
